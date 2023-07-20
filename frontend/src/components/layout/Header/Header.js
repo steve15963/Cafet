c5307b642c6187b94a9caa776bcc4e61b3420bb5
박동휘 <daniel1502@naver.com>
2023-07-20 16:06:28 +0900

    Update : Header css 수정

--- frontend/src/components/layout/Header/Header.js
+++ frontend/src/components/layout/Header/Header.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Header.css";
 
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -10,33 +8,50 @@ const Header = () => {
     <div>
       <nav className="Header_guest">
         <div class="header-container">
-          <Link to={"/"}>
-            <img src="images/logo192.png" alt="로고" class="logo" />
-          </Link>
-          <Link to={"/"}>
-            <img src="images/logo512.png" alt="메인로고" class="mainlogo" />
-          </Link>
-          <Link to={"/LoginPage"} class="login-link">
-            로그인
-          </Link>
-          &nbsp;&nbsp;
-          <Link to={"/LoginPage"} class="login-link">
-            회원가입
-          </Link>
-          &nbsp;&nbsp;
+          <div class="logo-container">
+            <Link to={"/"}>
+              <img src="images/logo192.png" alt="로고" class="logo" />
+            </Link>
+          </div>
+          <div class="mainlogo-container">
+            <Link to={"/"}>
+              <img src="images/logo512.png" alt="메인로고" class="mainlogo" />
+            </Link>
+          </div>
+          <div class="link-container">
+            <Link to={"/LoginPage"} class="link">
+              로그인
+            </Link>
+            &nbsp;&nbsp;
+            <Link to={"/LoginPage"} class="link">
+              회원가입
+            </Link>
+          </div>
         </div>
       </nav>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container className="d-flex justify-content-center w-100">
-          <Navbar.Brand href="#animalSearch">동물별로 찾기</Navbar.Brand>
-          |&nbsp;&nbsp;
-          <Navbar.Brand href="#shopSearch">가게별로 찾기</Navbar.Brand>
-          |&nbsp;&nbsp;
-          <Navbar.Brand href="#community">커뮤니티</Navbar.Brand>|&nbsp;&nbsp;
-          <Navbar.Brand href="#newsFeed">카페소식</Navbar.Brand>|&nbsp;&nbsp;
-          <Navbar.Brand href="#search">검색</Navbar.Brand>
-        </Container>
-      </Navbar>
+      <nav className="Header_category">
+        <div>
+          <Link to={"/LoginPage"} class="link">
+            동물별로 찾기
+          </Link>
+          &nbsp;&nbsp;| &nbsp;
+          <Link to={"/LoginPage"} class="link">
+            가게별로 찾기
+          </Link>
+          &nbsp;&nbsp;| &nbsp;
+          <Link to={"/LoginPage"} class="link">
+            커뮤니티
+          </Link>
+          &nbsp;&nbsp;| &nbsp;
+          <Link to={"/LoginPage"} class="link">
+            카페소식
+          </Link>
+          &nbsp;&nbsp;| &nbsp;
+          <Link to={"/LoginPage"} class="link">
+            검색
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 };
