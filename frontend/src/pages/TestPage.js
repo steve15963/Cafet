a3bfe865fe7b222903f66e1350bf24cfbcb49c2c
박동휘 <daniel1502@naver.com>
2023-07-20 12:50:38 +0900

    Update : 테스트 페이지에 Footer 구현

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import Header from "../components/layout/Header/Header";
+import Footer from "../components/layout/Footer/Footer";
+import Button from "../components/item/Button/Button";
+
 const TestPage = () => {
-  return <div>테스트 페이지</div>;
+  return (
+    <div>
+      <Header />
+      <Button
+        text={"버튼버튼"}
+        onClick={() => {
+          alert("클릭했습니다.");
+        }}
+      />
+      <Footer />
+    </div>
+  );
 };
 
 export default TestPage;
