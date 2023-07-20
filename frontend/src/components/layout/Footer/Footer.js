243808e5072d76bf7f9ba18ab38a6a02013fa3cf
박동휘 <daniel1502@naver.com>
2023-07-20 15:23:55 +0900

    Update :  footer 구현 완료

--- frontend/src/components/layout/Footer/Footer.js
+++ frontend/src/components/layout/Footer/Footer.js
@@ -1,25 +1,22 @@
 import React from "react";
 import "./Footer.css";
 
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container className="d-flex justify-content-center w-100">
-          <Navbar.Brand href="#">회사소개</Navbar.Brand>
-          |&nbsp;&nbsp;
-          <Navbar.Brand href="#">제휴제안</Navbar.Brand>
-          |&nbsp;&nbsp;
-          <Navbar.Brand href="#">이용약관</Navbar.Brand>
-          |&nbsp;&nbsp;
-          <Navbar.Brand href="#">개인정보처리방침</Navbar.Brand>
-          |&nbsp;&nbsp;
-          <Navbar.Brand href="#inquire">문의하기</Navbar.Brand>
-        </Container>
-      </Navbar>
+      <nav className="Footer">
+        <div class="footer-container">
+          <p class="text">
+            회사소개 | 제휴제안 | 이용약관 | 개인정보처리방침 |
+          </p>
+          &nbsp;
+          <Link to={"/LoginPage"} class="link">
+            문의하기
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 };
