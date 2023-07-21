89d3f015d60f284183be3946396d0bfeb5b3c1cd
김재석 <jaeseki@gmail.com>
2023-07-21 14:48:25 +0900

    feat: input 컴포넌트 생성

--- frontend/src/components/item/InputForm/InputForm.js
+++ frontend/src/components/item/InputForm/InputForm.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./InputForm.css";
 
 const InputForm = () => {
-  return <div></div>;
+  return (
+    <div>
+      <input
+        type=""
+        placeholder=""
+        className="inputTag"
+      />
+    </div>  
+  ) 
 };
 
 export default InputForm;
