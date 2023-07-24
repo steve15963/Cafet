178023b7fe9aa6e196617a0b36bef28ce07f137e
김재석 <jaeseki@gmail.com>
2023-07-24 17:33:11 +0900

    Feat: 전체 게시물 페이지 구현

--- frontend/src/pages/BoardPage.js
+++ frontend/src/pages/BoardPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import BoardDetail from "./BoardDetail"
+// import "/BoardPage.css"
 
-const BoardPage = () => {
-  return <div>게시판</div>;
+const BoardPage = ({ board }) => {
+  return (
+    <div className="BoardPage">
+      {
+        board.map((el) => {
+          return <BoardDetail {...el} />
+        })
+      }
+    </div>
+  );
 };
 
 export default BoardPage;
