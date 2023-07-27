d35ad230ded106a9e6bd39ed9685a954917f03e1
steve15963 <steve15963@gmail.com>
2023-07-27 17:14:49 +0900

    Style : 잠시 테스트 코드 비활성화

--- backend/src/test/java/xxx/petmanbe/PetManBeApplicationTests.java
+++ backend/src/test/java/xxx/petmanbe/PetManBeApplicationTests.java
@@ -3,7 +3,7 @@ package xxx.petmanbe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class PetManBeApplicationTests {
 
     @Test
