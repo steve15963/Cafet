858ed2708fd7cc3a89dad5c28753b390d3f35140
박동휘 <daniel1502@naver.com>
2023-07-27 17:33:28 +0900

    Design: css 중복 수정

--- backend/src/main/java/xxx/petmanbe/user/controller/UserController.java
+++ backend/src/main/java/xxx/petmanbe/user/controller/UserController.java
@@ -8,15 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
@@ -35,6 +27,7 @@ import xxx.petmanbe.userfile.service.FileService;
 @RestController
 @RequestMapping(value="/api/user")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class UserController {
 
 	private final UserService userService;
