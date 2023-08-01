323e5ee484b46cc99d984856a29c2eae626478c1
김재석 <jaeseki@gmail.com>
2023-08-01 17:43:18 +0900

    Feat: BoardPage 탭 별 보여주는 테이블 기능 구현, 페이지네이션 구현

--- frontend/src/pages/BoardPage/BoardPage.js
+++ frontend/src/pages/BoardPage/BoardPage.js
@@ -1,24 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { boardMenuList } from '../../hooks/useBoardPageMenu/useBoardPageMenu'
-import Menu from "../../components/Menu/Menu";
+
 import "./BoardPage.scoped.css";
+
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Button from "../../components/Button/Button";
 
+
 import axios from "axios";
 
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const createTable = (boardId, boardTitle, nickname, createdTime, viewCnt, commentSum) => {
   return {boardId, boardTitle, nickname, createdTime, viewCnt, commentSum}
@@ -26,8 +64,25 @@ const createTable = (boardId, boardTitle, nickname, createdTime, viewCnt, commen
 
 
 const BoardPage = () => {
-  // 메뉴 버튼 활성화를 위한 state 관리
-  // const [menuSelected, setMenuSelected] = useState(-1)
+
+  const [value, setValue] = React.useState(0);  // Tabs
+  const handleChange = (event, newValue) => {   // Tabs
+    setValue(newValue);
+  };
+
+
+  const [page, setPage] = useState(0);  // pagination
+  const [rowsPerPage, setRowsPerPage] = useState(10);  // pagination
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   let navigate = useNavigate();
 
   const goToDetail = (pageId) => {
@@ -50,62 +105,231 @@ const BoardPage = () => {
         console.log(error);
       });
   }, []);
-
-  const tableRows = boardData.map((el) => 
-    createTable(el.boardId, el.boardTitle, el.nickname, el.createdTime, el.viewCnt, el.commentSum)
-  )
+  
+  const totalBoard = boardData.map((el) => 
+    createTable(el.boardId, el.boardTitle, el.nickname, el.createdTime, el.viewCnt, el.commentSum, el.category_id)
+  )  // 전체게시판
+  const generalBoard = boardData.filter((it) => it.categoryName === "자유")
+  const questBoard = boardData.filter((it) => it.categoryName === "질문")
+  const adBoard = boardData.filter((it) => it.categoryName === "홍보")
 
   return (
     <>
       <Header />
       <div className="header-save" />
       <div className="menu_wrapper">
-        <div className="menu-left-save" />
-        <div className="menu_section">
-          {boardMenuList.map((it) => (
-            <Menu key={it.id} {...it} />
-          ))}
-        </div>
-        <div>
+      <div className="menu-left-save" />
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="전체게시판" {...a11yProps(0)} />
+            <Tab label="자유게시판" {...a11yProps(1)} />
+            <Tab label="질문게시판" {...a11yProps(2)} />
+            <Tab label="홍보게시판" {...a11yProps(3)} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <h1>전체 게시판</h1>
+          <div className="btn-wrapper">
+            <Button text={'글 작성'} onClick={goToCreate}/>
+          </div>
+          <Paper sx={{ width: '100%', overflow: 'hidden' }}>        
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">번호</TableCell>
+                    <TableCell align="center">제목</TableCell>
+                    <TableCell align="center">작성자</TableCell>
+                    <TableCell align="center">작성일자</TableCell>
+                    <TableCell align="center">조회 수</TableCell>
+                    <TableCell align="center">댓글 수</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {totalBoard
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => (
+                    <TableRow
+                      key={row.boardId}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row" align="center">
+                        {row.boardId}
+                      </TableCell>
+                      <TableCell onClick={() => goToDetail(row.boardId)}>{row.boardTitle}</TableCell>
+                      <TableCell>{row.nickname}</TableCell>
+                      <TableCell>{row.createdTime}</TableCell>
+                      <TableCell align="center">{row.viewCnt}</TableCell>
+                      <TableCell align="center">{row.commentSum}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 50]}
+              component="div"
+              count={totalBoard.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+        <h1>자유게시판</h1>
+        <div className="btn-wrapper">
           <Button text={'글 작성'} onClick={goToCreate}/>
         </div>
-        <div className="menu-right-save" />
-      </div>
-      <div className="table-wrapper">
-        <div className="table-left-save" />
-        
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">번호</TableCell>
-              <TableCell align="center">제목</TableCell>
-              <TableCell align="center">작성자</TableCell>
-              <TableCell align="center">작성일자</TableCell>
-              <TableCell align="center">조회 수</TableCell>
-              <TableCell align="center">댓글 수</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {tableRows.map((row) => (
-              <TableRow
-                key={row.boardId}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row" align="center">
-                  {row.boardId}
-                </TableCell>
-                <TableCell onClick={() => goToDetail(row.boardId)}>{row.boardTitle}</TableCell>
-                <TableCell>{row.nickname}</TableCell>
-                <TableCell>{row.createdTime}</TableCell>
-                <TableCell align="center">{row.viewCnt}</TableCell>
-                <TableCell align="center">{row.commentSum}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-        <div className="table-right-save" />
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>        
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">번호</TableCell>
+                  <TableCell align="center">제목</TableCell>
+                  <TableCell align="center">작성자</TableCell>
+                  <TableCell align="center">작성일자</TableCell>
+                  <TableCell align="center">조회 수</TableCell>
+                  <TableCell align="center">댓글 수</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {generalBoard
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => (
+                  <TableRow
+                    key={row.boardId}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row" align="center">
+                      {row.boardId}
+                    </TableCell>
+                    <TableCell onClick={() => goToDetail(row.boardId)}>{row.boardTitle}</TableCell>
+                    <TableCell>{row.nickname}</TableCell>
+                    <TableCell>{row.createdTime}</TableCell>
+                    <TableCell align="center">{row.viewCnt}</TableCell>
+                    <TableCell align="center">{row.commentSum}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 50]}
+            component="div"
+            count={generalBoard.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={2}>
+          <h1>질문게시판</h1>
+          <div className="btn-wrapper">
+            <Button text={'글 작성'} onClick={goToCreate}/>
+          </div>
+          <Paper sx={{ width: '100%', overflow: 'hidden' }}>        
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">번호</TableCell>
+                    <TableCell align="center">제목</TableCell>
+                    <TableCell align="center">작성자</TableCell>
+                    <TableCell align="center">작성일자</TableCell>
+                    <TableCell align="center">조회 수</TableCell>
+                    <TableCell align="center">댓글 수</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {questBoard
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => (
+                    <TableRow
+                      key={row.boardId}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row" align="center">
+                        {row.boardId}
+                      </TableCell>
+                      <TableCell onClick={() => goToDetail(row.boardId)}>{row.boardTitle}</TableCell>
+                      <TableCell>{row.nickname}</TableCell>
+                      <TableCell>{row.createdTime}</TableCell>
+                      <TableCell align="center">{row.viewCnt}</TableCell>
+                      <TableCell align="center">{row.commentSum}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 50]}
+              component="div"
+              count={questBoard.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={3}>
+          <h1>홍보게시판</h1>
+          <div className="btn-wrapper">
+            <Button text={'글 작성'} onClick={goToCreate}/>
+          </div>
+          <Paper sx={{ width: '100%', overflow: 'hidden' }}>        
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">번호</TableCell>
+                    <TableCell align="center">제목</TableCell>
+                    <TableCell align="center">작성자</TableCell>
+                    <TableCell align="center">작성일자</TableCell>
+                    <TableCell align="center">조회 수</TableCell>
+                    <TableCell align="center">댓글 수</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {adBoard
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => (
+                    <TableRow
+                      key={row.boardId}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row" align="center">
+                        {row.boardId}
+                      </TableCell>
+                      <TableCell onClick={() => goToDetail(row.boardId)}>{row.boardTitle}</TableCell>
+                      <TableCell>{row.nickname}</TableCell>
+                      <TableCell>{row.createdTime}</TableCell>
+                      <TableCell align="center">{row.viewCnt}</TableCell>
+                      <TableCell align="center">{row.commentSum}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 50]}
+              component="div"
+              count={adBoard.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </CustomTabPanel>
+      </Box>
+      <div className="menu-right-save" />
       </div>
       <div className="input_wrapper">
         <div className="group">
@@ -116,11 +340,6 @@ const BoardPage = () => {
             </g>
           </svg>
         </div>
-      </div>
-      <div className="pagenation">
-        <Stack>
-          <Pagination count={10} shape="rounded" />
-        </Stack>
       </div>
       <div className="footer-save" />
       <Footer />
