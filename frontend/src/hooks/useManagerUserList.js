33b71d4822af5b81e172215adcd9e34be2139040
박동휘 <daniel1502@naver.com>
2023-08-02 16:50:56 +0900

    Update : 오류 수정

--- frontend/src/hooks/useManagerUserList.js
+++ frontend/src/hooks/useManagerUserList.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useManagerUserList = () => {
-  const [managerUserList, setmanagerUserList] = useState([]);
+  const [managerUserList, setManagerUserList] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect((nameQuery, emailQuery, nicknameQuery) => {
@@ -11,7 +11,7 @@ const useManagerUserList = () => {
         const response = await axios.get(
           `http://i9a105.p.ssafy.io:8080/api/user?nickname=${nicknameQuery}&emailQuery=${emailQuery}`
         );
-        setmanagerUserList(response.data);
+        setManagerUserList(response.data);
         setLoading(false);
       } catch (error) {
         console.error("유저 목록을 불러오는 중 에러가 발생했습니다.", error);
