b2be099eb8bbb1c96c8f8e2086bb69fd849e3345
steve15963 <steve15963@gmail.com>
2023-08-02 17:51:03 +0900

    Feat : 거리 측정, 칼리브레이션, 등 원형 배열을 이용한 RSSI 평균값 계산. 평균 RSSI를 이용한 거리 계산 AT+DISI? 명령어 자동 삽입화.

--- Embedded/2023-08-01/2023-08-01.ino
+++ Embedded/2023-08-01/2023-08-01.ino
@@ -3,15 +3,42 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define MAX_CIRCULAR_SIZE 5
+#define BEACON_COUNT 4
+
 
 
 SoftwareSerial BTSerial(2, 3);  // SoftwareSerial(RX, TX)
 
 
-float calculateDistance(int rssi) {
-  float n = 2.0;
-  float constant = -62;
-  float distance = pow(10, (constant - rssi) / (10 * n));
+typedef struct {
+    int insertIndex;
+    int data[MAX_CIRCULAR_SIZE];
+} CircularArray;
+
+void InitCircularArray(CircularArray *target){
+  target->insertIndex = 0;
+}
+
+double getAvg(CircularArray *target){
+  int sum = 0;
+  for(int i = 0 ; i < MAX_CIRCULAR_SIZE;i++){
+    sum += target->data[i];
+  }
+  return sum / MAX_CIRCULAR_SIZE;
+}
+void setData(CircularArray *target,int data){
+  target->data[target->insertIndex] = data;
+  target->insertIndex = (target->insertIndex+1) % MAX_CIRCULAR_SIZE;
+}
+
+CircularArray Beacon[BEACON_COUNT];
+
+double calculateDistance(int rssi,int index) {
+  double offset[BEACON_COUNT] = {-53,-49,-49};
+  double n = 2.0;
+  double constant = offset[index-1];
+  double distance = pow(10, (constant - rssi) / (10 * n));
   return distance;
 }
 
@@ -28,16 +55,22 @@ void setup()
   // set the data rate for the BT port
 
   BTSerial.begin(9600);
+
+  for(int i = 0 ; i < BEACON_COUNT; i++){
+    InitCircularArray(&Beacon[i]);
+  }
   
 }
 
 void loop() {
   int i = 0;
+
   
+  int searchFlag[BEACON_COUNT] = {0,};
 
   while (BTSerial.available()) {  // if BT sends something
     String inputStr = BTSerial.readStringUntil('\n');
-    // Serial.println(inputStr);
+    //Serial.println(inputStr);
     char* response = inputStr.c_str();
     // Serial.println(data);
 
@@ -45,7 +78,7 @@ void loop() {
  
     char* data[6];
     if(strcmp(token,"OK+DISIS\r") == 0){
-      Serial.println("스캔 시작");
+      Serial.println("===============스캔 시작=============");
     }
     else if(strcmp(token,"OK+DISC") == 0){
       while (token != NULL) {
@@ -65,23 +98,45 @@ void loop() {
         Serial.println(atoi(storeNum));
         strncpy(beaconNum,data[2]+24,8);
         Serial.print("비콘 번호 : ");
-        Serial.println(atoi(beaconNum));      
+        int beaconIndex = atoi(beaconNum);
+        Serial.println(beaconIndex);  
         Serial.print("신호 세기 : ");
-        Serial.println(atoi(data[5]));
+
+        int power = atoi(data[5]);
+        Serial.println(power);
+        
+
+        setData(&Beacon[beaconIndex], power);
+
+        Serial.print("평균 신호 세기 :");
+        int avgPower = getAvg(&Beacon[beaconIndex]);
+        Serial.println(avgPower);
+
         Serial.print("거리 : ");
-        Serial.println(calculateDistance(atoi(data[5])));
+        Serial.println(calculateDistance(avgPower,beaconIndex));
+
+        searchFlag[beaconIndex] = 1;
+
+        if(searchFlag[1] == 1 && searchFlag[2] == 1 && searchFlag[3] == 1){
+          searchFlag[1] = searchFlag[2] = searchFlag[3] = 0;
+          Serial.println("위치계산 시작");
+
+        }
+
+        Serial.println("-------------------------------------");
+
       }
     }
     else if(strcmp(token,"OK+DISCE\r") == 0){
-      Serial.println("재검색");
+      Serial.println("================재검색=====================");
       BTSerial.println("AT+DISI?");
     }
     else{
       Serial.print("예외 발생 :");
       Serial.println(response);
       i = 0;
-      BTSerial.println("AT+RESET");
-      BTSerial.println("AT+DISI?");
+      BTSerial.println("AT+RESET\r");
+      BTSerial.println("AT+DISI?\r");
     }
   }
 
@@ -94,4 +149,3 @@ void loop() {
     BTSerial.write(data);  // write it to BT
   }
 }
-
