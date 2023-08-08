fea839d8c3ec7cfffef7f4e56bda008e6cd0a17d
박동휘 <daniel1502@naver.com>
2023-08-08 10:25:49 +0900

    Update : jwt 토큰 테스트

--- frontend/src/utils/handleLogin.js
+++ frontend/src/utils/handleLogin.js
@@ -1,6 +1,7 @@
 //login
 
 import axios from "axios";
+axios.defaults.withCredentials = true;
 
 const handleLogin = (email, password) => {
   const data = {
@@ -10,7 +11,14 @@ const handleLogin = (email, password) => {
 
   const serverUrl = "http://i9a105.p.ssafy.io/api/user/login";
 
-  return axios.post(serverUrl, data);
+  axios
+    .post(serverUrl, data)
+    .then((response) => {
+      const { accessToken } = response.data;
+      console.log(accessToken);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    })
+    .catch((error) => {});
 };
 
 export default handleLogin;
