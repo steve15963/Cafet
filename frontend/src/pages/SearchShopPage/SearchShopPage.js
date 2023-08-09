b09e6c5f28c6ec24154feb4ad9f976eaaa54dce3
김재석 <jaeseki@gmail.com>
2023-08-09 15:36:09 +0900

    Feat: 지도 검색 후 마커 및 마커 윈도우 구현

--- frontend/src/pages/SearchShopPage/SearchShopPage.js
+++ frontend/src/pages/SearchShopPage/SearchShopPage.js
@@ -1,9 +1,7 @@
-import { useState } from 'react';
-import { Map } from 'react-kakao-maps-sdk';
+import { useState, useEffect, useCallback } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 import axios from 'axios';
-
-// import useGeolocation from 'react-hook-geolocation';
 
 import './SearchShopPage.scoped.css'
 import Header from "../../components/Header/Header";
@@ -12,40 +10,43 @@ import SearchCard from '../../components/SearchCard/SearchCard';
 
 import Grid from '@mui/material/Grid';
 
-
-// const { kakao } = window;
-
-// const mockData = [
-//   {
-//     id: 0,
-//     shop: '고양이 다락방',
-//     lat: 37.555970,
-//     lng: 126.923110,
-//     petClassify: '고양이',
-//   },
-//   {
-//     id: 1,
-//     shop: '포포앤미루',
-//     lat: 37.525396,
-//     lng: 127.134214,
-//     petClassify: '강아지',
-//   },
-// ]
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const SearchShopPage = () => {
 
-  // const [animal, setAnimal] = useState('')
   const [region, setRegion] = useState('')
+  const [animal, setAnimal] = useState('')
   const [cafeList, setCafeList] = useState([])
+  
+  useEffect(() => {
+    axios.get('http://i9a105.p.ssafy.io:8080/api/shop')
+      .then(function(res) {
+        setCafeList(res.data.slice(0, 9))
+      })
+      .catch(function(err) {
+        console.log(err)
+      })
+  }, [])
 
-  const onChangeSearch = (e) => {
-    setRegion(e.target.value)
+  const handleAnimalChange = (e) => {
+    setAnimal(e.target.value)
   }
 
+  const onChangeSearch = useCallback((e) => {
+    const newValue = e.target.value;
+    setRegion(newValue);
+  }, []);
+
   const searchResult = (place) => {
+    if (place.trim() === "") return;
+
     axios.get(`http://i9a105.p.ssafy.io:8080/api/shop/address/${place}`)
       .then(function(res) {
-        setCafeList(res.data)
+        const newCafeList = res.data.map(el => ({ ...el })); // 새로운 배열 생성
+        setCafeList(newCafeList);
         setRegion("")
       })
       .catch(function(err) {
@@ -54,33 +55,10 @@ const SearchShopPage = () => {
   }
   console.log('cafeList :', cafeList)
 
-  // useEffect(() => {
-  //   let container = document.getElementById("map");
-  //   let options = {
-  //     center: new kakao.maps.LatLng(37.544293, 127.076089),
-  //     level: 7,
-  //   };
-
-  //   //map
-  //   const map = new kakao.maps.Map(container, options);
-    
-  //   mockData.forEach((el) => {
-  //     // 마커 생성
-  //     new kakao.maps.Marker({
-  //       //마커가 표시 될 지도
-  //       map: map,
-  //       //마커가 표시 될 위치
-  //       position: new kakao.maps.LatLng(el.lat, el.lng),
-  //       //마커에 hover시 나타날 title
-  //       title: el.shop,
-  //     });
-  //   });
-  // }, []);
-
-  // const moveCenter = () => {
-  //   let moveLatLon = new kakao.maps.LatLng(geolocation.latitude, geolocation.longitude);
-  //   // map.setCenter(moveLatLon)
-  // }
+  const handleClick = useCallback(() => {
+    searchResult(region);
+  }, [region]);
+   
   return (
     <div className='SearchShop'>
       <Header />
@@ -90,31 +68,59 @@ const SearchShopPage = () => {
           <div className='search-wrapper'>
             <div className='region-input'>
               <input value={region} onChange={onChangeSearch}/>
-              <button onClick={() => {searchResult(region)}}>검색</button>
+              <button onClick={handleClick}>검색</button>
             </div>
-          </div>
+              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel id="select-animal-label">Animal</InputLabel>
+                <Select
+                  labelId="select-animal-label"
+                  id="select-animal"
+                  value={animal}
+                  label="Animal"
+                  onChange={handleAnimalChange}
+                >
+                  <MenuItem value={'강아지'}>강아지</MenuItem>
+                  <MenuItem value={'고양이'}>고양이</MenuItem>
+                  <MenuItem value={'기타'}>기타</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
           <div className='list-wrapper'>
             {
               cafeList.map((el) => 
-                <SearchCard {...el} />
+                <SearchCard key={el.shopId} {...el} />
               )
             }
           </div>
         </Grid>
         <Grid item xs={6}>
-        <Map // 지도를 표시할 Container
-          center={{
-            // 지도의 중심좌표
-            lat: 33.450701,
-            lng: 126.570667,
-          }}
-          style={{
-            // 지도의 크기
-            width: "100%",
-            height: "450px",
-          }}
-          level={3} // 지도의 확대 레벨
-        />
+          <Map // 지도를 표시할 Container
+            center={{
+              // 지도의 중심좌표
+              lat: 37.566971,
+              lng: 126.978439,
+            }}
+            style={{
+              // 지도의 크기
+              width: "100%",
+              height: "450px",
+            }}
+            level={7} // 지도의 확대 레벨
+          >
+            {
+              cafeList.map((cafe) => 
+                <MapMarker
+                  key={cafe.shopId}
+                  position={{ lat: cafe.latitude, lng: cafe.longitude }}
+                  removable={true}
+                >
+                  <div style={{ padding: "5px", color: "#000", textAlign: 'center' }}>
+                    {cafe.shopTitle}
+                  </div>
+                </MapMarker>
+              )
+            }
+          </Map>
         </Grid>
       </Grid>
       <div className="footer-save"/>
