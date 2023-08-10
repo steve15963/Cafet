00eb68b0be6530d2e667341c7d3a4679957c7d0e
donggeoniiii <pkgun0727@gmail.com>
2023-08-10 10:38:45 +0900

    Feat: CommentResponseDto 수정 필드값에 boardTitle 추가

--- backend/src/main/java/xxx/petmanbe/comment/dto/response/CommentResponseDto.java
+++ backend/src/main/java/xxx/petmanbe/comment/dto/response/CommentResponseDto.java
@@ -12,6 +12,7 @@ import xxx.petmanbe.comment.entity.Comment;
 public class CommentResponseDto {
 	private Long commentId;
 	private Long userId;
+	private String boardTitle;
 	private String content;
 	private String nickname;
 	private LocalDateTime createdTime;
@@ -22,6 +23,7 @@ public class CommentResponseDto {
 	public CommentResponseDto(Comment comment) {
 		this.commentId = comment.getCommentId();
 		this.userId = comment.getUser().getUserId();
+		this.boardTitle = comment.getBoard().getBoardTitle();
 		this.content = comment.getContent();
 		this.nickname = comment.getUser().getNickname();
 		this.createdTime = comment.getCreatedTime();
