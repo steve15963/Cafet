8bd02098365592e4a3885b6eeb4081db4cd4882e
박동휘 <daniel1502@naver.com>
2023-08-10 12:59:53 +0900

    Fix : api 경로 수정

--- frontend/src/hooks/useUserList.js
+++ frontend/src/hooks/useUserList.js
@@ -3,36 +3,21 @@ import axios from "axios";
 
 const useUserList = () => {
   const [userList, setUserList] = useState([]);
-  //eslint-disable-next-line
-  const [searchEmail, setsearchEmail] = useState(true);
-  //eslint-disable-next-line
-  const [searchNickname, setsearchNickname] = useState(true);
+  const [query, setQuery] = useState({ email: "", nickname: "" });
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const getUserList = async () => {
-      try {
-        //   "https://i9a105.p.ssafy.io/api/user"
-        const response = await axios.get("https://i9a105.p.ssafy.io/api/user");
-        setUserList(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error("유저 목록을 불러오는 중 에러가 발생했습니다.", error);
-        setLoading(false);
-      }
-    };
-
-    getUserList();
-  }, []);
 
   useEffect(() => {
     setLoading(true);
     const getUserList = async () => {
       try {
+        let url = `https://i9a105.p.ssafy.io/api/user`;
         //   "https://i9a105.p.ssafy.io/api/user"
-        const response = await axios.get(
-          `https://i9a105.p.ssafy.io/api/user/email/${searchEmail}`
-        );
+        if (query.email) {
+          url += `/email/${query.email}`;
+        } else if (query.nickname) {
+          url += `/nickname/${query.nickname}`;
+        }
+        const response = await axios.get(url);
         setUserList(response.data);
         setLoading(false);
       } catch (error) {
@@ -42,9 +27,9 @@ const useUserList = () => {
     };
 
     getUserList();
-  }, [searchEmail, searchNickname]);
+  }, [query]);
 
-  return { userList, searchEmail, searchNickname, loading };
+  return { userList, setQuery, loading };
 };
 
 export default useUserList;
