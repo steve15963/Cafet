10bcb536df72149f0750b6e698f9e896f483f44f
steve15963 <steve15963@gmail.com>
2023-08-11 17:34:07 +0900

    Feat : Controller 추가.

--- backend/src/main/java/xxx/petmanbe/Location/controller/LocationController.java
+++ backend/src/main/java/xxx/petmanbe/Location/controller/LocationController.java
@@ -2,20 +2,46 @@ package xxx.petmanbe.Location.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import xxx.petmanbe.Location.dto.requestDto.AddPetLocationRequestDto;
+import xxx.petmanbe.Location.dto.requestDto.HitMapRequestDto;
+import xxx.petmanbe.Location.dto.responseDto.HitMapResponseDto;
+import xxx.petmanbe.Location.dto.responseDto.PetLocationResponseDto;
+import xxx.petmanbe.Location.entity.PetLocation;
+import xxx.petmanbe.Location.service.HitMapService;
+import xxx.petmanbe.Location.service.LocationService;
+import xxx.petmanbe.Location.service.LocationServiceImpl;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/location")
+@AllArgsConstructor
 public class LocationController {
 
-	@PostMapping("/api/location/pet")
-	public ResponseEntity<String> SavePetLocation(@RequestBody AddPetLocationRequestDto addPetLocationRequestDto){
+	private final LocationService locationService;
+
+	private final HitMapService hitMapService;
+
+	@PostMapping("/pet")
+	public ResponseEntity<PetLocationResponseDto> SavePetLocation(@RequestBody AddPetLocationRequestDto addPetLocationRequestDto){
 		log.info(addPetLocationRequestDto.toString());
-		return new ResponseEntity<>("", HttpStatus.ACCEPTED);
+		PetLocation petLocation = locationService.getTrilateration(addPetLocationRequestDto);
+		return new ResponseEntity<>(
+			new PetLocationResponseDto(petLocation),
+			petLocation == null ?HttpStatus.BAD_REQUEST: HttpStatus.ACCEPTED
+		);
+	}
+
+	@GetMapping("/pet")
+	public ResponseEntity<HitMapResponseDto> GetHitMap(@RequestBody HitMapRequestDto hitMapRequestDto){
+		HitMapResponseDto hitMap = hitMapService.getHitMap(hitMapRequestDto.getAnimalId());
+		return new ResponseEntity<>(hitMap,HttpStatus.OK);
 	}
 }
