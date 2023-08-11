f87da099ae2688f3ac51684a0470c4d928b4127d
박동휘 <daniel1502@naver.com>
2023-08-11 16:50:16 +0900

    Update :로그아웃 구현 방식 수정 테스트

--- frontend/src/utils/handleLogout.js
+++ frontend/src/utils/handleLogout.js
@@ -1,12 +1,12 @@
 //login
 
-import axios from "axios";
+import axiosCreate from "axios";
 
 const handleLogout = () => {
   console.log("handleLogout");
   const serverUrl = "https://i9a105.p.ssafy.io/api/user/logout";
 
-  return axios.post(serverUrl);
+  return axiosCreate.post(serverUrl);
 };
 
 export default handleLogout;
