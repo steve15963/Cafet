b6f4301217452fa3ce7f2cd6431ed4d600a3158c
donggeoniiii <pkgun0727@gmail.com>
2023-08-11 10:37:13 +0900

    Feat: 에러코드 추가

--- backend/src/main/java/xxx/petmanbe/exception/errorcode/CommonErrorCode.java
+++ backend/src/main/java/xxx/petmanbe/exception/errorcode/CommonErrorCode.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
 	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "INVALID PARAMETER INCLUDED: 유효하지 않은 파라미터 값"),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR: 서버 에러")
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR: 서버 에러"),
+	BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD REQUEST: 잘못된 요청")
 	;
 
 	private final HttpStatus httpStatus;
