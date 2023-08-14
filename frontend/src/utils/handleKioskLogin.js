17c2aaac9dcb6ac9b3577274cdf01bb1865192d2
박동휘 <daniel1502@naver.com>
2023-08-14 16:36:02 +0900

    Fix : 로그인 수정

--- frontend/src/utils/handleKioskLogin.js
+++ frontend/src/utils/handleKioskLogin.js
@@ -1,6 +1,6 @@
 //login
 
-import axiosCreate from "../axiosCreate";
+import axios from "axios";
 
 const handleKioskLogin = (email, password) => {
   console.log("handleKioskLogin");
@@ -11,7 +11,7 @@ const handleKioskLogin = (email, password) => {
 
   const serverUrl = "https://i9a105.p.ssafy.io/api/kiosk/user/login";
 
-  return axiosCreate.post(serverUrl, data);
+  return axios.post(serverUrl, data);
 };
 
 export default handleKioskLogin;
