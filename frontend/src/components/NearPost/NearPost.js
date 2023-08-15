4dea98dde3f0782770e6ccd3b036a4785502559d
Park Dong Hui <daniel1502@naver.com>
2023-08-15 17:23:33 +0900

    Update : 이동경로도 추가

--- frontend/src/components/NearPost/NearPost.js
+++ frontend/src/components/NearPost/NearPost.js
@@ -1,6 +1,7 @@
 //이전글과 다음글로 이동하기 위한 component(수정 필요)
 import React from "react";
 import "./NearPost.scoped.css";
+import { useNavigate } from "react-router-dom";
 
 const NearPost = () => {
   const prevId = sessionStorage.getItem('prevId');
@@ -16,20 +17,27 @@ const NearPost = () => {
   const nextNickname = sessionStorage.getItem('nextNickname');
   sessionStorage.removeItem('nextNickname');
 
+  let navigate = useNavigate();
+  
+  const goToDetail = (pageId) => {
+    navigate(`/board/detail/${pageId}`);
+  };
+  
+
   return (
     <div>
     <ul>
     {nextId !== "null" && (
         <div className="nearpost">
           <div className="nearpost_left">{"다음 글"}</div>
-          <div className="nearpost_center">{nextTitle}</div>
+          <div className="nearpost_center" onClick={() => goToDetail(nextId)} >{nextTitle}</div>
           <div className="nearpost_right">{nextNickname}</div>
         </div>
       )}
       {prevId !== "null" && (
         <div className="nearpost">
           <div className="nearpost_left">{"이전 글"}</div>
-          <div className="nearpost_center">{prevTitle}</div>
+          <div className="nearpost_center" onClick={() => goToDetail(prevId)}>{prevTitle}</div>
           <div className="nearpost_right">{prevNickname}</div>
         </div>
       )}
