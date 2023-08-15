bc5e7c98699b314aeb2b73eed524c2fa42931019
Park Dong Hui <daniel1502@naver.com>
2023-08-15 14:30:34 +0900

    Update : 이전글 다음글 수정 + console.log 추가

--- frontend/src/components/NearPost/NearPost.js
+++ frontend/src/components/NearPost/NearPost.js
@@ -1,40 +1,28 @@
 //이전글과 다음글로 이동하기 위한 component(수정 필요)
 import React from "react";
 import "./NearPost.scoped.css";
+import useBoardDetail from "../../hooks/useBoardDetail";
 
 const NearPost = (props) => {
   const { prevId, nextId } = props;
+  const { prevDetail } = useBoardDetail(prevId);
+  const { nextDetail } = useBoardDetail(nextId);
+console.log(prevDetail);
+console.log(nextDetail);
 
-  const mockPost = [
-    {
-      comment_id: prevId,
-      title: "우리집 고양이",
-      created_time: new Date().getTime(),
-      updated_time: "2022-04-06",
-      board_id: "후기",
-      text: "이전글",
-    },
-    {
-      comment_id: nextId,
-      title: "역삼 동물 카페 추천",
-      created_time: new Date().getTime(),
-      updated_time: "2022-04-06",
-      board_id: "후기",
-      text: "다음글",
-    },
-  ];
   return (
     <div>
       <ul>
-        {mockPost.map((value) => (
-          <li key={value.comment_id}>
-            <div className="nearpost">
-              <div className="nearpost_left">{value.text}</div>
-              <div className="nearpost_center">{value.title}</div>
-              <div className="nearpost_right">{value.created_time}</div>
-            </div>
-          </li>
-        ))}
+        <div className="nearpost">
+          <div className="nearpost_left">{prevDetail.text}</div>
+          <div className="nearpost_center">{prevDetail.title}</div>
+          <div className="nearpost_right">{prevDetail.created_time}</div>
+        </div>
+        <div className="nearpost">
+          <div className="nearpost_left">{prevDetail.text}</div>
+          <div className="nearpost_center">{prevDetail.title}</div>
+          <div className="nearpost_right">{prevDetail.created_time}</div>
+        </div>
       </ul>
     </div>
   );
