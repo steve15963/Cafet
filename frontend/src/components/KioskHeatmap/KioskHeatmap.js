89541491614f3a04659be70592f1e18b9afa7a74
김재석 <jaeseki@gmail.com>
2023-08-16 14:39:30 +0900

    Feat: 가게 팔로우 기능 구현

--- frontend/src/components/KioskHeatmap/KioskHeatmap.js
+++ frontend/src/components/KioskHeatmap/KioskHeatmap.js
@@ -1,27 +1,41 @@
 import axios from "axios"
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import h337 from 'heatmap.js'
 
 const KioskHeatmap = ({ id }) => {
 
+  // const [heatmapData, setHeatmapData] = useState([
+  //   { x: 100, y: 200, value: 5 },
+  //   { x: 200, y: 300, value: 10 },
+  // ])
+
   useEffect(() => {
     // Heatmap 데이터를 생성합니다. 데이터 형식은 x, y, value를 포함해야 합니다.
-    const data = [
-      { x: 100, y: 200, value: 5 },
-      { x: 200, y: 300, value: 10 },
-      // 추가 데이터...
-    ];
+    
+    axios.get(`https://i9a105.p.ssafy.io/api/location/pet?animalId=${id}`)
+      .then(function(res) {
+        const heatmapInstance = h337.create({
+          container: document.getElementById('heatmapContainer'),
+        });
+        heatmapInstance.setData(res.data);
+      })
+      .catch(function(err) {
+        console.log(err)
+      })
+  }, [id]);
 
-    // Heatmap 생성 및 설정
-    const heatmapInstance = h337.create({
-      container: document.getElementById('heatmapContainer'),
-    });
+  // useEffect(() => {
 
-    heatmapInstance.setData({
-      max: 20, // 최대값
-      data,
-    });
-  }, []);
+  //   // Heatmap 생성 및 설정
+  //   const heatmapInstance = h337.create({
+  //     container: document.getElementById('heatmapContainer'),
+  //   });
+
+  //   heatmapInstance.setData({
+  //     max: 10, // 최대값
+  //     heatmapData,
+  //   });
+  // }, [heatmapData])
 
   return ( 
     <div id="heatmapContainer" style={{ width: '100%', height: '400px' }} />
