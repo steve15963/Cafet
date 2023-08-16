42f60adee72fade139d23fca1105e091004c3964
Park Dong Hui <daniel1502@naver.com>
2023-08-17 02:35:53 +0900

    Update: 이전글 다음글 다시 테스트

--- frontend/src/components/NearPost/NearPost.js
+++ frontend/src/components/NearPost/NearPost.js
@@ -1,43 +1,49 @@
-//이전글과 다음글로 이동하기 위한 component(수정 필요)
 import React from "react";
 import "./NearPost.scoped.css";
 import { useNavigate } from "react-router-dom";
 
-const NearPost = ({boardList, currentId}) => {
-
+const NearPost = ({ boardList, currentId }) => {
   let navigate = useNavigate();
-  
+
   const goToDetail = (pageId) => {
     navigate(`/board/detail/${pageId}`);
   };
 
   const currentIndex = boardList.findIndex((post) => post.id === currentId);
-  console.log(currentId);
-  console.log(boardList);
-  
-  const prevId = currentIndex < boardList.length - 1 ? boardList[currentIndex + 1].boardId : null;
-  const nextId = currentIndex > 0 ? boardList[currentIndex - 1].boardId : null;
-  
+
+  const prevPost = currentIndex > 0 ? boardList[currentIndex - 1] : null;
+  const nextPost =
+    currentIndex < boardList.length - 1 ? boardList[currentIndex + 1] : null;
+
   return (
     <div>
-    <ul>
-    {nextId !== "null" && (
-        <div className="nearpost">
-          <div className="nearpost_left">{"다음 글"}</div>
-          <div className="nearpost_center" onClick={() => goToDetail(nextId)} >{boardList[currentIndex - 1].boardTitle}</div>
-          <div className="nearpost_right">{boardList[currentIndex - 1].nickname}</div>
-        </div>
-      )}
-      {prevId !== "null" && (
-        <div className="nearpost">
-          <div className="nearpost_left">{"이전 글"}</div>
-          <div className="nearpost_center" onClick={() => goToDetail(prevId)}>{boardList[currentIndex + 1].boardTitle}</div>
-          <div className="nearpost_right">{boardList[currentIndex + 1].nickname}</div>
-        </div>
-      )}
-    </ul>
-  </div>
-  
+      <ul>
+        {nextPost && (
+          <div className="nearpost">
+            <div className="nearpost_left">{"다음 글"}</div>
+            <div
+              className="nearpost_center"
+              onClick={() => goToDetail(nextPost.id)}
+            >
+              {nextPost.boardTitle}
+            </div>
+            <div className="nearpost_right">{nextPost.nickname}</div>
+          </div>
+        )}
+        {prevPost && (
+          <div className="nearpost">
+            <div className="nearpost_left">{"이전 글"}</div>
+            <div
+              className="nearpost_center"
+              onClick={() => goToDetail(prevPost.id)}
+            >
+              {prevPost.boardTitle}
+            </div>
+            <div className="nearpost_right">{prevPost.nickname}</div>
+          </div>
+        )}
+      </ul>
+    </div>
   );
 };
 
