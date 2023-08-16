eb2d68848b785a1ff104d434b0d32bd6343074d4
박동휘 <daniel1502@naver.com>
2023-08-16 15:09:23 +0900

    Update : 팔로우 테스트

--- frontend/src/components/ShopFollow/ShopFollow.js
+++ frontend/src/components/ShopFollow/ShopFollow.js
@@ -1,70 +1,64 @@
-import { useState } from "react"
-import axios from "axios"
+import { useState } from "react";
+import axios from "axios";
 
-import IconButton from '@mui/material/IconButton';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import IconButton from "@mui/material/IconButton";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
-const ShopFollow = ({ userId, shopId, isFollowing, followState}) => {
-  const [ isLoading, setIsLoading ] = useState(false)
-  const [ btnStatus, setBtnStatus ] = useState(false)
- 
+const ShopFollow = ({ userId, shopId, isFollowing, followState }) => {
+  const [btnStatus, setBtnStatus] = useState(false);
+
   const handleFollowFeat = () => {
     if (isFollowing) {
-      setBtnStatus(false)
+      setBtnStatus(false);
     } else {
-      setBtnStatus(!true)
+      setBtnStatus(true);
     }
 
-    if (isLoading) {  // 로딩 중이라면
-      return          // 함수 종료
-    }
-    setIsLoading(true);  // 로딩 중이라고 변경
-
-    if (isFollowing) {  // 팔로잉 중이라면 == 언팔로우 한다는 것
-      axios.delete(`https://i9a105.p.ssafy.io/api/shop/like`, {
-        data: {
-          userId,
-          shopId,
-        }
-      })
-      .then(() => {
-        followState(false)
-      })
-      .catch(error => {
-        console.log('delete error')
-        console.log(error)
-      })
-      .finally(() => {
-        setIsLoading(false)
-      })
+    if (isFollowing) {
+      // 팔로잉 중이라면 == 언팔로우 한다는 것
+      axios
+        .delete(`https://i9a105.p.ssafy.io/api/shop/like`, {
+          data: {
+            userId,
+            shopId,
+          },
+        })
+        .then(() => {
+          followState(false);
+        })
+        .catch((error) => {
+          console.log("delete error");
+          console.log(error);
+        });
     } else {
-      axios.post(`https://i9a105.p.ssafy.io/api/shop/like`, {
-          userId,
-          shopId,
-      })
-      .then(() => {
-        followState(true)
-      })
-      .catch(error => {
-        console.log('post error')
-        console.log(error)
-      })
-      .finally(() => {
-        setIsLoading(false)
-      })
+      axios
+        .post(`https://i9a105.p.ssafy.io/api/shop/like`, {
+          data: {
+            userId,
+            shopId,
+          },
+        })
+        .then(() => {
+          followState(true);
+        })
+        .catch((error) => {
+          console.log("post error");
+          console.log(error);
+        });
     }
-  }
- return (
-  <div>
-    <IconButton onClick={handleFollowFeat}>
-      {
-        btnStatus ? <BookmarkIcon color="primary" /> : 
-                      <BookmarkBorderIcon color="primary"/>
-      }
-    </IconButton>
-  </div>
- )
-}
+  };
+  return (
+    <div>
+      <IconButton onClick={handleFollowFeat}>
+        {btnStatus ? (
+          <BookmarkIcon color="primary" />
+        ) : (
+          <BookmarkBorderIcon color="primary" />
+        )}
+      </IconButton>
+    </div>
+  );
+};
 
-export default ShopFollow
+export default ShopFollow;
