cf17d66eed0541404808a18c38e7cab450b158a5
Park Dong Hui <daniel1502@naver.com>
2023-08-18 04:11:01 +0900

    Update :임시저장

--- frontend/src/components/KioskNameList/KioskNameList.js
+++ frontend/src/components/KioskNameList/KioskNameList.js
@@ -15,11 +15,20 @@ const KioskNameList = () => {
   console.log(nameList);
   
   const handleDeskClick = (deskId, deskNum) => {
-
     localStorage.setItem("selectedDeskId", deskId);
     localStorage.setItem("selectedDeskNum", deskNum);
     
-    navigate(`/kiosk/menu/${shopId}}`);
+    navigate(`/kiosk/menu/${shopId}`);
+  };
+
+  const handleEditClick = (deskId) => {
+    // Handle edit button click, e.g., navigate to edit page
+    console.log(`Edit button clicked for deskId: ${deskId}`);
+  };
+
+  const handleDeleteClick = (deskId) => {
+    // Handle delete button click, e.g., show a confirmation modal
+    console.log(`Delete button clicked for deskId: ${deskId}`);
   };
 
   if (nameList.length === 0) {
@@ -29,14 +38,30 @@ const KioskNameList = () => {
   return (
     <div className="kiosk-list-container">
       <p className="login-form-title">키오스크 리스트</p>
-      <br/>
+      <br />
       {nameList.map(item => (
-        <div
-          key={item.deskId}
-          className="kiosk-list-item"
-          onClick={() => handleDeskClick(item.deskId, item.deskNum)}
-        >
-          Desk {item.deskId}: (Number {item.deskNum})
+        <div key={item.deskId} className="kiosk-list-item">
+          <div
+            key={item.deskId}
+            className="kiosk-list-item"
+            onClick={() => handleDeskClick(item.deskId, item.deskNum)}
+          >
+            Desk {item.deskId}: (Number {item.deskNum})
+          </div>
+          <div className="button-container">
+            <button
+              className="edit-button"
+              onClick={() => handleEditClick(item.deskId)}
+            >
+              수정
+            </button>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteClick(item.deskId)}
+            >
+              삭제
+            </button>
+          </div>
         </div>
       ))}
     </div>
