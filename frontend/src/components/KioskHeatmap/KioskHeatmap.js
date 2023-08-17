c8e7f1c848c4e15dd38a3da179dd16ddd03d4f4e
박동휘 <daniel1502@naver.com>
2023-08-17 14:32:19 +0900

    Fix : 테스트를 위한 수정

--- frontend/src/components/KioskHeatmap/KioskHeatmap.js
+++ frontend/src/components/KioskHeatmap/KioskHeatmap.js
@@ -6,20 +6,18 @@ import h337 from "heatmap.js";
 const KioskHeatmap = ({ id }) => {
   const [heatmapData, setHeatmapData] = useState([]);
 
-  const fetchHeatmapData = () => {
-    axios
-      .get(`https://i9a105.p.ssafy.io/api/location/pet?animalId=${id}`)
-      .then(function (res) {
-        res.data.max = 10;
-        setHeatmapData(res.data);
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  };
-
   useEffect(() => {
-    console.log("페이지 리로드"); // 페이지 로드될 때 "페이지 리로드" 출력
+    const fetchHeatmapData = () => {
+      axios
+        .get(`https://i9a105.p.ssafy.io/api/location/pet?animalId=${id}`)
+        .then(function (res) {
+          res.data.max = 10;
+          setHeatmapData(res.data);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    };
 
     fetchHeatmapData();
 
@@ -28,7 +26,7 @@ const KioskHeatmap = ({ id }) => {
     return () => {
       clearInterval(intervalId);
     };
-  }, []); // 의존성 배열을 빈 배열로 설정하여 워닝 무시
+  }, [id]);
 
   useEffect(() => {
     if (heatmapData.length > 0) {
