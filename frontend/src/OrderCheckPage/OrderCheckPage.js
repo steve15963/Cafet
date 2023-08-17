9174aa08698c2743c38433b4623920b340ff178d
이제원 <ljw4287@naver.com>
2023-08-18 05:54:36 +0900

    Update frontend/src/OrderCheckPage/OrderCheckPage.js

--- frontend/src/OrderCheckPage/OrderCheckPage.js
+++ frontend/src/OrderCheckPage/OrderCheckPage.js
@@ -23,8 +23,8 @@ const OrderCheckPage = () => {
     setMessage(updatedMenuList)
   }
 
-  // var sock = new SockJS('https://i9a105.p.ssafy.io/order')
-  var sock = new SockJS('http://localhost:8080/order')
+   var sock = new SockJS('https://i9a105.p.ssafy.io/order')
+  //var sock = new SockJS('http://localhost:8080/order')
   let client = Stomp.over(sock);
 
   useEffect(() => {
