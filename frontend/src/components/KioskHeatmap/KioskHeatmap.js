3f02652560c88155b1702b6e3ad5fb679b4cfac8
박동휘 <daniel1502@naver.com>
2023-08-17 14:26:53 +0900

    Update : 시연을 위한 임시 기능

--- frontend/src/components/KioskHeatmap/KioskHeatmap.js
+++ frontend/src/components/KioskHeatmap/KioskHeatmap.js
@@ -1,32 +1,53 @@
-import axios from "axios"
-import { useEffect } from "react"
-
-import "./KioskHeatmap.scoped.css"
-
-import h337 from 'heatmap.js'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./KioskHeatmap.scoped.css";
+import h337 from "heatmap.js";
 
 const KioskHeatmap = ({ id }) => {
+  const [heatmapData, setHeatmapData] = useState([]);
+
+  const fetchHeatmapData = () => {
+    axios
+      .get(`https://i9a105.p.ssafy.io/api/location/pet?animalId=${id}`)
+      .then(function (res) {
+        res.data.max = 10;
+        setHeatmapData(res.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
-    // Heatmap 데이터를 생성합니다. 데이터 형식은 x, y, value를 포함해야 합니다.
-    
-    axios.get(`https://i9a105.p.ssafy.io/api/location/pet?animalId=${id}`)
-      .then(function(res) {
-        res.data.max = 10
-        const heatmapInstance = h337.create({
-          container: document.getElementById('heatmapContainer'),
-        });
-        heatmapInstance.setData(res.data);
-      })
-      .catch(function(err) {
-        console.log(err)
-      })
-  }, [id]);
+    fetchHeatmapData();
 
-  return ( 
-    <div id="heatmapContainer" style={{ 
-      display: "flex", maxWidth: '600px', width: '100%', height: '400px' 
-    }} />
-  )
-}
-export default KioskHeatmap
+    const intervalId = setInterval(fetchHeatmapData, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []); // 의존성 배열을 빈 배열로 설정하여 워닝 무시
+
+  useEffect(() => {
+    if (heatmapData.length > 0) {
+      const heatmapInstance = h337.create({
+        container: document.getElementById("heatmapContainer"),
+      });
+      heatmapInstance.setData(heatmapData);
+    }
+  }, [heatmapData]);
+
+  return (
+    <div
+      id="heatmapContainer"
+      style={{
+        display: "flex",
+        maxWidth: "600px",
+        width: "100%",
+        height: "400px",
+      }}
+    />
+  );
+};
+
+export default KioskHeatmap;
